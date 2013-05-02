7ef3a32cae32fd4b9038c973fb31e586cf1f4d4c
Mike Guarino <michaelguarino@Michaels-MacBook-Pro-2.local>
2013-05-01 23:48:01 -0400

    Clean up
    removed all the compilation garbage from the repo

--- CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/michaelguarino/ShapePopulationViewer
-# Build directory: /Users/michaelguarino/ShapePopulationViewer
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(Testing/filePathTest "/Users/michaelguarino/ShapePopulationViewer/Testing/filePathTest")
-ADD_TEST(Testing/mapperTest "/Users/michaelguarino/ShapePopulationViewer/Testing/mapperTest")
-ADD_TEST(Testing/qVectorTests "/Users/michaelguarino/ShapePopulationViewer/Testing/qVectorTests")
