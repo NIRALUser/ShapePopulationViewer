7e399e7756d42231f30fd70b261721a293674dc8
Laura PASCAL <laurajulia.pascal@gmail.com>
2015-09-20 20:41:33 -0400

    PERF: Update version with the new options

---
+++ src/valueDirectionColorMapStruct.h
@@ -0,0 +1,18 @@
+#ifndef VALUEDIRECTIONCOLORMAPSTRUCT_H
+#define VALUEDIRECTIONCOLORMAPSTRUCT_H
+
+
+struct valueDirectionColorMapStruct
+{
+    double norm;
+
+    double max[3];
+    double min[3];
+
+    double maxAbs[3];
+    double minAbs[3];
+
+
+};
+
+#endif
