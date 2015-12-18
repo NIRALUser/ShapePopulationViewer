bf5742ed11af55be2d9278303c90bc908d524cc6
Laura PASCAL <laurajulia.pascal@gmail.com>
2015-12-18 14:05:00 -0500

    PERF: Creation of a test for the function setBackgroundSelectedColor in the class ShapePopulationBase

---
+++ src/Testing/testBackgroundSelectedColor.h
@@ -0,0 +1,16 @@
+#ifndef TESTBACKGROUNDSELECTEDCOLOR_H
+#define TESTBACKGROUNDSELECTEDCOLOR_H
+
+
+#include "../src/ShapePopulationBase.h"
+#include <math.h>
+
+class TestShapePopulationBase
+{
+public:
+    TestShapePopulationBase();
+
+    bool testBackgroundSelectedColor(std::string filename);
+};
+
+#endif // TESTBACKGROUNDSELECTEDCOLOR_H
