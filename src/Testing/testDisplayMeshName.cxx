ab03aa7da2d77dc25b8339d5f7f5339ad2240d78
Laura PASCAL <laurajulia.pascal@gmail.com>
2015-12-18 14:54:56 -0500

    PERF: Creation of a test for the function displayMeshName in the class ShapePopulationBase

---
+++ src/Testing/testDisplayMeshName.cxx
@@ -0,0 +1,70 @@
+
+#include "testDisplayMeshName.h"
+#include "QSharedPointer.h"
+#include "ShapePopulationQT.h"
+
+TestShapePopulationBase::TestShapePopulationBase()
+{
+
+}
+
+bool TestShapePopulationBase::testDisplayMeshName(std::string filename)
+{
+    QSharedPointer<ShapePopulationBase> shapePopulationBase = QSharedPointer<ShapePopulationBase>( new ShapePopulationBase );
+
+    int nbMesh = 2;
+    shapePopulationBase->m_windowsList.clear();
+    for(int i = 0; i < nbMesh; i++)
+    {
+        shapePopulationBase->CreateNewWindow(filename);
+    }
+
+    for(int i = 0; i < nbMesh; i++)
+    {
+        vtkSmartPointer<vtkPropCollection> propCollection =  shapePopulationBase->m_windowsList[i]->GetRenderers()->GetFirstRenderer()->GetViewProps();
+        vtkObject * viewPropObject = propCollection->GetItemAsObject(2);
+        vtkSmartPointer<vtkCornerAnnotation> cornerAnnotation = vtkSmartPointer<vtkCornerAnnotation>::New();
+        cornerAnnotation = (vtkCornerAnnotation*) viewPropObject;
+        if(!cornerAnnotation->GetVisibility()) return 1;
+    }
+
+    /// TEST 1.
+    /// Call the fonction to set the mesh name not visible
+    /// Wanted result: mesh name is not visible
+
+    // Call of the function that must be test
+    shapePopulationBase->displayMeshName(false);
+
+    // Test if the result obtained is correct:
+    for(int i = 0; i < nbMesh; i++)
+    {
+        vtkSmartPointer<vtkPropCollection> propCollection =  shapePopulationBase->m_windowsList[i]->GetRenderers()->GetFirstRenderer()->GetViewProps();
+        vtkObject * viewPropObject = propCollection->GetItemAsObject(2);
+        vtkSmartPointer<vtkCornerAnnotation> cornerAnnotation = vtkSmartPointer<vtkCornerAnnotation>::New();
+        cornerAnnotation = (vtkCornerAnnotation*) viewPropObject;
+        if(cornerAnnotation->GetVisibility()) return 1;
+    }
+
+    /// TEST 2.
+    /// Call the fonction to set the mesh name not visible
+    /// Wanted result: mesh name is not visible
+
+    // Call of the function that must be test
+    shapePopulationBase->displayMeshName(true);
+
+    // Test if the result obtained is correct:
+    for(int i = 0; i < nbMesh; i++)
+    {
+        vtkSmartPointer<vtkPropCollection> propCollection =  shapePopulationBase->m_windowsList[i]->GetRenderers()->GetFirstRenderer()->GetViewProps();
+        vtkObject * viewPropObject = propCollection->GetItemAsObject(2);
+        vtkSmartPointer<vtkCornerAnnotation> cornerAnnotation = vtkSmartPointer<vtkCornerAnnotation>::New();
+        cornerAnnotation = (vtkCornerAnnotation*) viewPropObject;
+        if(!cornerAnnotation->GetVisibility()) return 1;
+    }
+
+    return 0;
+}
+
+
+
+
---
+++ src/Testing/testDisplayMeshName.h
@@ -0,0 +1,16 @@
+#ifndef TESTDISPLAYMESHNAME_H
+#define TESTDISPLAYMESHNAME_H
+
+
+#include "../src/ShapePopulationBase.h"
+#include <math.h>
+
+class TestShapePopulationBase
+{
+public:
+    TestShapePopulationBase();
+
+    bool testDisplayMeshName(std::string filename);
+};
+
+#endif // TESTDISPLAYMESHNAME_H
