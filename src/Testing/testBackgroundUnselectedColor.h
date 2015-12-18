6a3c2293e6c2db92b0c12b7c9154f8bf2b61d63e
Laura PASCAL <laurajulia.pascal@gmail.com>
2015-12-18 14:11:53 -0500

    PERF: Creation of a test for the function setBackgroundUnselectedColor in the class ShapePopulationBase

---
+++ src/Testing/testBackgroundUnselectedColor.h
@@ -0,0 +1,16 @@
+#ifndef TESTBACKGROUNDUNSELECTEDCOLOR_H
+#define TESTBACKGROUNDUNSELECTEDCOLOR_H
+
+
+#include "../src/ShapePopulationBase.h"
+#include <math.h>
+
+class TestShapePopulationBase
+{
+public:
+    TestShapePopulationBase();
+
+    bool testBackgroundUnselectedColor(std::string filename);
+};
+
+#endif // TESTBACKGROUNDUNSELECTEDCOLOR_H
