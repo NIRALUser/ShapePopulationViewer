cd72e71777a005ea4e3c69f016f2e4d32d089d9e
Laura PASCAL <laurajulia.pascal@gmail.com>
2015-12-18 13:52:57 -0500

    PERF: Creation of a test for the function computeCommonRange in the class ShapePopulationBase

---
+++ src/Testing/testComputeCommonRange.cxx
@@ -0,0 +1,35 @@
+#include "testComputeCommonRange.h"
+#include "QSharedPointer.h"
+#include "ShapePopulationQT.h"
+
+TestShapePopulationBase::TestShapePopulationBase()
+{
+
+}
+
+bool TestShapePopulationBase::testComputeCommonRange(std::string filename)
+{
+    QSharedPointer<ShapePopulationBase> shapePopulationBase = QSharedPointer<ShapePopulationBase>( new ShapePopulationBase );
+
+    shapePopulationBase->m_selectedIndex.clear();
+    shapePopulationBase->m_selectedIndex.push_back(0);
+    shapePopulationBase->m_windowsList.clear();
+    shapePopulationBase->CreateNewWindow(filename);
+
+    std::ostringstream strs;
+    strs.str("");
+    strs.clear();
+    strs << "PointToPointVector_mag" << std::endl;
+
+    // Call of the function that must be test
+    double *commonRange = shapePopulationBase->computeCommonRange(strs.str().c_str(),shapePopulationBase->m_selectedIndex);
+
+    // Test if the result obtained is correct
+    if(fabs(commonRange[1] - 5.69522) > 0.00001 ) return 1;
+    if(fabs(commonRange[0] - 0.25032) > 0.00001 ) return 1;
+    return 0;
+}
+
+
+
+
---
+++ src/Testing/testComputeCommonRange.h
@@ -0,0 +1,16 @@
+#ifndef TESTCOMPUTECOMMONRANGE_H
+#define TESTCOMPUTECOMMONRANGE_H
+
+
+#include "../src/ShapePopulationBase.h"
+#include <math.h>
+
+class TestShapePopulationBase
+{
+public:
+    TestShapePopulationBase();
+
+    bool testComputeCommonRange(std::string filename);
+};
+
+#endif // TESTCOMPUTECOMMONRANGE_H
