NIRALUser/ShapePopulationViewer

commit bf3ac57ebd01f93720a9f8e8063e91c0e34bd038

ENH: Build Slicer module without GPL code from "gradientWidgetQT"
This commit replaces "ShapePopulationGradientWidgetQT.h" header now
referenced in the ShapePopulationQT.ui file.
* If the option ShapePopulationViewer_BUILD_SLICER_EXTENSION is ON,
  it derives from "ctkVTKScalarsToColorsWidget" and exclude GPL
  code.
* Otherwise it derives from "gradientWidgetQT" when built as a standalone
  CLI.

src/ShapePopulationQT.cxx
@@ -102,10 +102,15 @@ ShapePopulationQT::ShapePopulationQT(QWidget* parent) : QWidget(parent)
 #else
     connect(m_exportActions,SIGNAL(triggered(QAction*)),this,SLOT(showNoExportWindow()));
 #endif
+    pushButton_VISU_add->setVisible(false);
+    pushButton_VISU_delete->setVisible(false);
+
     //gradView Signals
     connect(gradientWidget_VISU,SIGNAL(arrowMovedSignal(qreal)), this, SLOT(slot_gradArrow_moved(qreal)));
     connect(gradientWidget_VISU,SIGNAL(arrowSelectedSignal(qreal)), this, SLOT(slot_gradArrow_selected(qreal)));
+#ifndef ShapePopulationViewer_BUILD_SLICER_EXTENSION
     connect(gradientWidget_VISU,SIGNAL(arrowDoubleClickedSignal()), this, SLOT(slot_gradArrow_doubleClicked()));
+#endif
     connect(gradientWidget_VISU,SIGNAL(noSelectionSignal()), this, SLOT(slot_no_gradArrow_selected()));
 
     //backgroundDialog signals
@@ -209,7 +214,7 @@ void ShapePopulationQT::loadModel(vtkMRMLModelNode* modelNode)
 
     CreateWidgets(renderWindows);
 #else
-    Q_ASSERT(modelNode);
+    Q_UNUSED(modelNode);
 #endif
 }
 
@@ -1718,6 +1723,11 @@ void ShapePopulationQT::on_comboBox_VISU_attribute_currentIndexChanged(int)
         // Change the colorbar selected
         m_usedColorBar = m_colorBarList[index]; //the colorbar depends of the attribute
         this->gradientWidget_VISU->setAllColors(&m_usedColorBar->colorPointList);
+
+#ifdef ShapePopulationViewer_BUILD_SLICER_EXTENSION
+        this->gradientWidget_VISU->setFocusIndex(1);
+#endif
+
         m_noChange = true;
         spinBox_VISU_min->setValue(m_usedColorBar->range[0]);
         spinBox_VISU_max->setValue(m_usedColorBar->range[1]);
@@ -1953,7 +1963,7 @@ void ShapePopulationQT::on_pushButton_VISU_resetRange_Dir_clicked()
     m_noChange = false;
 }
 
-
+#ifndef ShapePopulationViewer_BUILD_SLICER_EXTENSION
 void ShapePopulationQT::on_pushButton_VISU_delete_clicked()
 {
     gradientWidget_VISU->deleteFocusArrow();
@@ -1961,6 +1971,7 @@ void ShapePopulationQT::on_pushButton_VISU_delete_clicked()
     if(gradientWidget_VISU->getNumberOfArrows() <= 2) pushButton_VISU_delete->setDisabled(true);
     this->updateColorbar_QT();
 }
+#endif
 
 void ShapePopulationQT::on_spinBox_VISU_position_valueChanged(double arg1)
 {
@@ -1979,6 +1990,7 @@ void ShapePopulationQT::on_spinBox_VISU_position_valueChanged(double arg1)
     this->updateColorbar_QT();
 }
 
+#ifndef ShapePopulationViewer_BUILD_SLICER_EXTENSION
 void ShapePopulationQT::on_pushButton_VISU_add_clicked()
 {
     QColor color;
@@ -1989,6 +2001,7 @@ void ShapePopulationQT::on_pushButton_VISU_add_clicked()
         this->updateColorbar_QT();
     }
 }
+#endif
 
 void ShapePopulationQT::on_pushButton_VISU_reset_clicked()
 {
@@ -2061,7 +2074,7 @@ void ShapePopulationQT::slot_gradArrow_selected(qreal newPos)
     this->spinBox_VISU_position->setFocus();
     this->spinBox_VISU_position->selectAll();
 }
-
+#ifndef ShapePopulationViewer_BUILD_SLICER_EXTENSION
 void ShapePopulationQT::slot_gradArrow_doubleClicked()
 {
     QColor color = gradientWidget_VISU->getFocusColor();
@@ -2073,7 +2086,7 @@ void ShapePopulationQT::slot_gradArrow_doubleClicked()
 
     this->updateColorbar_QT();
 }
-
+#endif
 void ShapePopulationQT::slot_no_gradArrow_selected()
 {
     pushButton_VISU_delete->setDisabled(true);

src/CMakeLists.txt
@@ -27,6 +27,7 @@ set(CXX_FILES
   ShapePopulationBase.cxx
   ShapePopulationColorMapIO.cxx
   ShapePopulationData.cxx
+  ShapePopulationGradientWidgetQT.cxx
   ShapePopulationMainWindowQT.cxx
   ShapePopulationQT.cxx
   vtkPVPostFilter.cxx
@@ -48,6 +49,7 @@ set(QT_WRAP
   CSVloaderQT.h
   customizeColorMapByDirectionDialogQT.h
   gradientWidgetQT.h
+  ShapePopulationGradientWidgetQT.h
   ShapePopulationMainWindowQT.h
   ShapePopulationQT.h
   )
@@ -59,7 +61,11 @@ set(QT_QRC_FILES
 # --- LIBRARIES ---------------------------------------------------------------------------
 if(ShapePopulationViewer_QT_VERSION VERSION_EQUAL "4")
   QT4_WRAP_UI(UISrcs ${UI_FILES})
-  QT4_WRAP_CPP(MOCSrcs ${QT_WRAP})
+  set(_moc_options)
+  if(ShapePopulationViewer_BUILD_SLICER_EXTENSION)
+    set(_moc_options OPTIONS -DShapePopulationViewer_BUILD_SLICER_EXTENSION)
+  endif()
+  QT4_WRAP_CPP(MOCSrcs ${QT_WRAP} ${_moc_options})
   QT4_ADD_RESOURCES(QRCrcs ${QT_QRC_FILES})
 else()
   QT5_WRAP_UI(UISrcs ${UI_FILES})
@@ -75,6 +81,7 @@ add_library(${LOCAL_PROJECT_NAME}Widget STATIC
 target_link_libraries(${LOCAL_PROJECT_NAME}Widget PUBLIC
   ${QT_LIBRARIES}
   ${VTK_LIBRARIES}
+  $<$<BOOL:${ShapePopulationViewer_BUILD_SLICER_EXTENSION}>:CTKVisualizationVTKWidgets>
   )
 
 if(ShapePopulationViewer_BUILD_SLICER_EXTENSION)

src/ShapePopulationGradientWidgetQT.cxx
@@ -1,0 +1,218 @@
+
+// ShapePopulationViewer includes
+#include "ShapePopulationColorMapIO.h"
+#include "ShapePopulationGradientWidgetQT.h"
+
+// CTK includes
+#ifdef ShapePopulationViewer_BUILD_SLICER_EXTENSION
+#include <ctkTransferFunction.h>
+#include <ctkTransferFunctionGradientItem.h>
+#include <ctkTransferFunctionControlPointsItem.h>
+#include <ctkVTKColorTransferFunction.h>
+#include <ctkVTKScalarsToColorsView.h>
+
+// VTK includes
+#include <vtkAxis.h>
+#include <vtkColorTransferFunction.h>
+#include <vtkContextScene.h>
+#include <vtkControlPointsItem.h>
+#include <vtkPlot.h>
+#include <vtkSmartPointer.h>
+
+// Qt includes
+#include <QDebug>
+#include <QDoubleSpinBox>
+#include <QHBoxLayout>
+
+#endif
+
+class ShapePopulationGradientWidgetQTPrivate
+{
+public:
+    ShapePopulationGradientWidgetQTPrivate();
+
+#ifdef ShapePopulationViewer_BUILD_SLICER_EXTENSION
+    vtkSmartPointer<vtkColorTransferFunction> CTF;
+    QDoubleSpinBox* XSpinBox;
+#endif
+};
+
+ShapePopulationGradientWidgetQTPrivate::ShapePopulationGradientWidgetQTPrivate()
+{
+}
+
+ShapePopulationGradientWidgetQT::ShapePopulationGradientWidgetQT(QWidget* parent)
+    : Superclass(parent), d_ptr( new ShapePopulationGradientWidgetQTPrivate )
+{
+#ifdef ShapePopulationViewer_BUILD_SLICER_EXTENSION
+    Q_D(ShapePopulationGradientWidgetQT);
+    this->setHorizontalSliderVisible(false);
+    this->setVerticalSliderVisible(false);
+    this->setTopWidgetsVisible(false);
+
+    d->CTF = vtkSmartPointer<vtkColorTransferFunction>::New();
+    this->view()->addColorTransferFunction(d->CTF);
+
+    this->qvtkConnect(this->currentControlPointsItem(), vtkControlPointsItem::CurrentPointChangedEvent,
+                      this, SLOT(onCurrentPointChangedEvent(vtkObject*)));
+    this->qvtkConnect(this->currentControlPointsItem(), vtkCommand::ModifiedEvent,
+                      this, SLOT(onCurrentPointModifiedEvent(vtkObject*)));
+
+    this->reset();
+#endif
+}
+
+ShapePopulationGradientWidgetQT::~ShapePopulationGradientWidgetQT()
+{
+}
+
+#ifdef ShapePopulationViewer_BUILD_SLICER_EXTENSION
+
+void ShapePopulationGradientWidgetQT::loadColorPointList(QString a_filePath, std::vector<colorPointStruct> * a_colorPointList)
+{
+    ShapePopulationColorMapIO::loadColorPointList(a_filePath, a_colorPointList, this);
+    this->setAllColors(a_colorPointList);
+}
+
+void ShapePopulationGradientWidgetQT::saveColorPointList(QString a_filePath)
+{
+    //Get ColorPointList
+    std::vector<colorPointStruct> colorPointList;
+    getAllColors(&colorPointList);
+    ShapePopulationColorMapIO::saveColorPointList(a_filePath, colorPointList, this);
+}
+
+bool ShapePopulationGradientWidgetQT::getAllColors(std::vector<colorPointStruct> * colorPointList)
+{
+    Q_D(ShapePopulationGradientWidgetQT);
+    colorPointList->clear();
+
+    for(int idx = 0; idx < d->CTF->GetSize(); ++idx)
+    {
+        double nodeValue[6];
+        d->CTF->GetNodeValue(idx, nodeValue);
+        colorPointStruct colorPoint(nodeValue[0], nodeValue[1], nodeValue[2], nodeValue[3]);
+        colorPointList->push_back(colorPoint);
+    }
+    return d->CTF->GetSize() > 0;
+}
+
+void ShapePopulationGradientWidgetQT::setAllColors(std::vector<colorPointStruct> * colorPointList)
+{
+    Q_D(ShapePopulationGradientWidgetQT);
+    d->CTF->RemoveAllPoints();
+    for(unsigned int idx = 0 ; idx < colorPointList->size() ; idx++)
+    {
+        colorPointStruct colorPoint = colorPointList->at(idx);
+        d->CTF->AddRGBPoint(colorPoint.pos, colorPoint.r, colorPoint.g, colorPoint.b);
+    }
+}
+
+void ShapePopulationGradientWidgetQT::setFocusPosition(const qreal newPos)
+{
+    vtkIdType currentPointId = this->currentControlPointsItem()->GetCurrentPoint();
+    double point[4] = {0.0};
+    this->currentControlPointsItem()->GetControlPoint(currentPointId, point);
+    point[0] = newPos;
+    this->currentControlPointsItem()->SetControlPoint(currentPointId, point);
+}
+
+qreal ShapePopulationGradientWidgetQT::getFocusPosition()
+{
+    vtkIdType currentPointId = this->currentControlPointsItem()->GetCurrentPoint();
+    if (currentPointId == -1)
+    {
+        return 0.0;
+    }
+    double point[4] = {0.0};
+    this->currentControlPointsItem()->GetControlPoint(currentPointId, point);
+    return point[0];
+}
+
+void ShapePopulationGradientWidgetQT::setFocusIndex(int index)
+{
+    if (index < -1 || index >= this->currentControlPointsItem()->GetNumberOfPoints())
+    {
+        return;
+    }
+    this->currentControlPointsItem()->SetCurrentPoint(index);
+}
+
+int ShapePopulationGradientWidgetQT::getFocusIndex()
+{
+    return static_cast<int>(this->currentControlPointsItem()->GetCurrentPoint());
+}
+
+void ShapePopulationGradientWidgetQT::enable(std::vector<colorPointStruct> *a_colorPointList)
+{
+    this->setEnabled(true);
+    this->setAllColors(a_colorPointList);
+}
+
+void ShapePopulationGradientWidgetQT::disable()
+{
+    this->setEnabled(false);
+}
+
+int ShapePopulationGradientWidgetQT::getNumberOfArrows()
+{
+    Q_D(ShapePopulationGradientWidgetQT);
+    return d->CTF->GetSize();
+}
+
+void ShapePopulationGradientWidgetQT::reset()
+{
+    Q_D(ShapePopulationGradientWidgetQT);
+    d->CTF->RemoveAllPoints();
+
+    double range_min = 0.0;
+    double range_max = 1.0;
+    double center = (range_max - range_min) / 2.0;
+
+    // Elements: {leftMin, leftMax, bottomMin, bottomMax, rightMin, rightMax, topMin, topMax}
+    // (probably according to vtkAxis::Location)
+    double chartBounds[8] = {0.0};
+    this->view()->chartBounds(chartBounds);
+    chartBounds[2] = range_min;
+    chartBounds[3] = range_max;
+    this->view()->setChartUserBounds(chartBounds);
+    this->view()->update();
+    this->view()->setAxesToChartBounds();
+
+    {
+        QColor color = Qt::green;
+        d->CTF->AddRGBPoint(range_min, color.redF(), color.greenF(), color.blueF());
+    }
+    {
+        QColor color = Qt::yellow;
+        d->CTF->AddRGBPoint(center, color.redF(), color.greenF(), color.blueF());
+    }
+    {
+        QColor color = Qt::red;
+        d->CTF->AddRGBPoint(range_max, color.redF(), color.greenF(), color.blueF());
+    }
+}
+
+void ShapePopulationGradientWidgetQT::onCurrentPointChangedEvent(vtkObject* caller)
+{
+    vtkControlPointsItem* controlPoints = vtkControlPointsItem::SafeDownCast(caller);
+    long newPoint = this->getFocusIndex();
+    if (!controlPoints || newPoint <= -1)
+        {
+        emit noSelectionSignal();
+        return;
+        }
+    emit arrowSelectedSignal(this->getFocusPosition());
+}
+
+void ShapePopulationGradientWidgetQT::onCurrentPointModifiedEvent(vtkObject* caller)
+{
+    vtkControlPointsItem* controlPoints = vtkControlPointsItem::SafeDownCast(caller);
+    long newPoint = this->getFocusIndex();
+    if (!controlPoints || newPoint <= -1)
+      {
+      return;
+      }
+    emit arrowMovedSignal(this->getFocusPosition());
+}
+#endif

src/ShapePopulationGradientWidgetQT.h
@@ -1,0 +1,76 @@
+#ifndef ShapePopulationGradientWidgetQT_h
+#define ShapePopulationGradientWidgetQT_h
+
+#include "ShapePopulationStruct.h"
+#include "ShapePopulationViewerConfig.h"
+
+#ifdef ShapePopulationViewer_BUILD_SLICER_EXTENSION
+# include <ctkVTKScalarsToColorsWidget.h>
+#else
+# include "gradientWidgetQT.h"
+#endif
+
+// Qt includes
+#include <QColor>
+
+// STD includes
+#include <vector>
+
+class ShapePopulationGradientWidgetQTPrivate;
+
+class ShapePopulationGradientWidgetQT :
+#ifdef ShapePopulationViewer_BUILD_SLICER_EXTENSION
+    public ctkVTKScalarsToColorsWidget
+#else
+    public gradientWidgetQT
+#endif
+{
+    Q_OBJECT
+public:
+#ifdef ShapePopulationViewer_BUILD_SLICER_EXTENSION
+    typedef ctkVTKScalarsToColorsWidget Superclass;
+#else
+    typedef gradientWidgetQT Superclass;
+#endif
+    ShapePopulationGradientWidgetQT(QWidget* parent=0);
+    virtual ~ShapePopulationGradientWidgetQT();
+
+#ifdef ShapePopulationViewer_BUILD_SLICER_EXTENSION
+    void loadColorPointList(QString a_filePath, std::vector<colorPointStruct> * a_colorPointList);
+    void saveColorPointList(QString a_filePath);
+
+    bool getAllColors(std::vector<colorPointStruct> * colorPointList);
+    void setAllColors(std::vector<colorPointStruct> * colorPointList);
+
+    void setFocusPosition(const qreal newPos);
+    qreal getFocusPosition();
+
+    void setFocusIndex(int index);
+    int getFocusIndex();
+
+    void enable(std::vector<colorPointStruct> *a_colorPointList);
+    void disable();
+
+    int getNumberOfArrows();
+
+public slots:
+    void reset();
+    void onCurrentPointChangedEvent(vtkObject* controlPointsItem);
+    void onCurrentPointModifiedEvent(vtkObject* controlPointsItem);
+
+signals:
+    void arrowMovedSignal(const qreal pos);
+    void arrowSelectedSignal(const qreal pos);
+    void noSelectionSignal();
+
+#endif
+
+protected:
+    QScopedPointer<ShapePopulationGradientWidgetQTPrivate> d_ptr;
+private:
+    Q_DECLARE_PRIVATE(ShapePopulationGradientWidgetQT)
+    Q_DISABLE_COPY(ShapePopulationGradientWidgetQT);
+};
+
+
+#endif

src/ShapePopulationQT.h
@@ -198,8 +198,10 @@ protected:
     void on_pushButton_VISU_resetRange_Dir_clicked();
     // arrows of the gradient widget
     void on_spinBox_VISU_position_valueChanged(double arg1);
+#ifndef ShapePopulationViewer_BUILD_SLICER_EXTENSION
     void on_pushButton_VISU_delete_clicked();
     void on_pushButton_VISU_add_clicked();
+#endif
     void on_pushButton_VISU_reset_clicked();
     // color map
     void on_radioButton_displayColorMapByMagnitude_toggled(bool checked);
@@ -208,7 +210,9 @@ protected:
     //slots for gradView signals
     void slot_gradArrow_moved(qreal newPos);
     void slot_gradArrow_selected(qreal newPos);
+#ifndef ShapePopulationViewer_BUILD_SLICER_EXTENSION
     void slot_gradArrow_doubleClicked();
+#endif
     void slot_no_gradArrow_selected();
 
     //VECTORS

src/ShapePopulationQT.ui
@@ -789,7 +789,7 @@
                    </item>
                   </layout>
                  </widget>
-                 <widget class="gradientWidgetQT" name="gradientWidget_VISU" native="true">
+                 <widget class="ShapePopulationGradientWidgetQT" name="gradientWidget_VISU" native="true">
                   <property name="geometry">
                    <rect>
                     <x>0</x>
@@ -1714,9 +1714,9 @@ p, li { white-space: pre-wrap; }
  </widget>
  <customwidgets>
   <customwidget>
-   <class>gradientWidgetQT</class>
+   <class>ShapePopulationGradientWidgetQT</class>
    <extends>QWidget</extends>
-   <header>gradientWidgetQT.h</header>
+   <header>ShapePopulationGradientWidgetQT.h</header>
    <container>1</container>
   </customwidget>
  </customwidgets>

src/ShapePopulationStruct.h
@@ -33,6 +33,15 @@ struct cameraConfigStruct
 
 struct colorPointStruct
 {
+    colorPointStruct()
+        : pos(0.), r(0.), g(0.), b(0.)
+    {
+    }
+    colorPointStruct(double _pos, double _r, double _g, double _b)
+        : pos(_pos), r(_r), g(_g), b(_b)
+    {
+    }
+
     double pos;
     double r;
     double g;

commit a422dbfbeea4f9ed258f679cd874b20f215a9d50

BUG: Ensure add/remove arrow buttons are visible in standalone CLI
This commit fixes a regression introduced in bf3ac57 (ENH: Build Slicer
module without GPL code from "gradientWidgetQT")

src/ShapePopulationQT.cxx
@@ -102,8 +102,10 @@ ShapePopulationQT::ShapePopulationQT(QWidget* parent) : QWidget(parent)
 #else
     connect(m_exportActions,SIGNAL(triggered(QAction*)),this,SLOT(showNoExportWindow()));
 #endif
+#ifdef ShapePopulationViewer_BUILD_SLICER_EXTENSION
     pushButton_VISU_add->setVisible(false);
     pushButton_VISU_delete->setVisible(false);
+#endif
 
     //gradView Signals
     connect(gradientWidget_VISU,SIGNAL(arrowMovedSignal(qreal)), this, SLOT(slot_gradArrow_moved(qreal)));