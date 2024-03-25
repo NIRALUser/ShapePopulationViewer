03461f0d545ae3b41d0b91755423a4731ec0ffe9
Ye Han <ye.han@kitware.com>
2024-03-25 17:29:41 -0400

    ENH: make csv loader accept new s-rep format

--- src/CSVloaderQT.cxx
+++ src/CSVloaderQT.cxx
@@ -55,13 +55,13 @@ void CSVloaderQT::on_buttonBox_accepted()
     for (int i = 0; i < fileList.size(); i++)
     {
         QString QFilePath = fileList[i].absoluteFilePath();
-        if (!QFilePath.endsWith(".vtk") && !QFilePath.endsWith(".vtp") && !QFilePath.endsWith(".xml"))
+        if (!QFilePath.endsWith(".vtk") && !QFilePath.endsWith(".vtp") && !QFilePath.endsWith(".xml") && !QFilePath.endsWith(".srep.json"))
         {
             fileList.removeAt(i);
             i--;
             std::ostringstream strs;
             strs << QFilePath.toStdString() << std::endl
-                 << "This is not a vtk/vtp/xml file."<< std::endl;
+                 << "This is not a vtk/vtp/xml/srep.json file."<< std::endl;
             QMessageBox::critical(this,"Wrong file format",QString(strs.str().c_str()), QMessageBox::Ok);
         }
         else if(!fileList[i].exists())
